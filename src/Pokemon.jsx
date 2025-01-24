893bfae42da9e3f31d9d45cbeb2916d13c48ef06
Sagantpss <sagantpss@gmail.com>
2025-01-24 19:23:01 +0530

    limit changes to 100

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -7,7 +7,7 @@ export const Pokemon = () => {
   const [error, setError] = useState(null);
   const [search, setSearch] = useState("");
 
-  const API = "https://pokeapi.co/api/v2/pokemon?limit=1000";
+  const API = "https://pokeapi.co/api/v2/pokemon?limit=100";
 
   const fetchPokemon = async () => {
     try {
